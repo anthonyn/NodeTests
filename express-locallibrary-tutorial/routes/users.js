17c2e8f703ca5c46cc395b0e011eb671afaf87c5
anthonyn <anthony.nicolaides@gmail.com>
2018-02-20 18:02:23 -0500

    end of chap 2

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/sandwich', function(req, res, next) {
+  res.send('respond with a sandwich');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send("You're so cool");
+});
+
 module.exports = router;
